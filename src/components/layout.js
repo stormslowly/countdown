e5c9032fc4008db03a9e0b53f7383c4e71e78e3e
stormslowly@gmail.com <stormslowly@gmail.com>
2018-12-28 13:57:51 +0800

    🎨 a basic version with figlet as service

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,44 +1,29 @@
-import React from 'react'
 import PropTypes from 'prop-types'
-import { StaticQuery, graphql } from 'gatsby'
-
-import Header from './header'
+import React from 'react'
 import './layout.css'
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
+const Layout = ({children}) => (
+    <div
+        style={{
+            margin: `30px auto`,
             maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
+            padding: `px 1.0875rem 1.45rem`,
             paddingTop: 0,
-          }}
-        >
-          {children}
-          <footer>
-            © 2018, Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
-    )}
-  />
+            border: 'black solid 1px',
+            minHeight: '600px',
+            position: 'relative'
+        }}
+    >
+        {children}
+        <footer style={{position: 'absolute', bottom: 0, right: 0}}>
+            made with ❤️ by pshu
+        </footer>
+    </div>
+
 )
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+    children: PropTypes.node.isRequired,
 }
 
 export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,92 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import * as PropTypes from "prop-types";
+import React, {Component, useEffect, useState} from 'react'
+import * as request from 'superagent'
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 
 import Layout from '../components/layout'
-import Image from '../components/image'
-import SEO from '../components/seo'
+
+const Y2019 = new Date('2019-01-01 00:00:00').getTime()
+
+class CountDownTimer extends Component {
+
+    interval = null
+
+    state = {
+        seconds: 99999
+    }
+
+    updateCountdown = () => {
+        const now = Date.now()
+        const s = Math.round((Y2019 - now) / 1000)
+
+        this.setState({seconds: s > 0 ? s : 0})
+    }
+
+    componentWillMount() {
+        this.interval = setInterval(() => {
+            this.updateCountdown()
+        }, 1000)
+
+        this.updateCountdown()
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval)
+    }
+
+    render() {
+        let {children} = this.props;
+        return children({seconds: this.state.seconds})
+    }
+}
+
+CountDownTimer.propTypes = {children: PropTypes.func}
+
+const figletLoading = "  _                 _ _                 \n | | ___   __ _  __| (_)_ __   __ _     \n | |/ _ \\ / _` |/ _` | | '_ \\ / _` |    \n | | (_) | (_| | (_| | | | | | (_| |_ _ \n |_|\\___/ \\__,_|\\__,_|_|_| |_|\\__, (_|_)\n                              |___/     "
+
+
+const FigLetService = ({text, font, children}) => {
+
+    const [figlet, setFiglet] = useState(figletLoading)
+
+
+    useEffect(() => {
+        request.get('https://figlet.shupengfei.me')
+            .query({text, font})
+            .then(({body}) => {
+                setFiglet(body.figlet)
+            })
+    }, [text, font])
+
+
+    return children({figlet})
+}
+
+
+const GridFiglet = ({figlet}) => {
+
+    return <pre>{figlet}</pre>
+
+}
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+    <Layout>
+
+        <CountDownTimer>{
+            ({seconds}) => {
+                return <React.Fragment>
+
+                    <FigLetService text={seconds}>{({figlet}) => {
+                        return <GridFiglet figlet={figlet}/>
+                    }}</FigLetService>
+
+                    <FigLetService text={'happy  new  year'}>
+                        {({figlet}) => <GridFiglet figlet={figlet}></GridFiglet>}
+                    </FigLetService>
+                </React.Fragment>
+            }
+        }</CountDownTimer>
+    </Layout>
 )
 
 export default IndexPage
